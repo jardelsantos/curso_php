b613b337d5c5a3dc330714f4f276fbdda8f22dd7
Jardel Santos <jardel.santos@vonex.com.br>
2022-06-21 21:49:21 -0300

    Sintaxe
    Sintaxe Básica do PHP

---
+++ base_sintaxe.php
@@ -0,0 +1,32 @@
+<?php
+
+echo 'Parada para o café!.';
+
+$nomeVariavel = 22;
+
+define('NOME_CONSTANTE', 10);
+
+// Comentario de uma linha
+
+# COMENTARIO DE UMA LINHA
+
+/*
+Varias linhas
+de comentarios
+*/
+
+/*
+OPERADORES ARITMETICOS
++ - * / %
+
+OPERADORES RELACIONAIS
+> < == !=
+
+OPERADORES LOGICOS
+AND OR NOT 
+*/
+
+// TERMINAL
+$variavel = readline();
+
+?>
